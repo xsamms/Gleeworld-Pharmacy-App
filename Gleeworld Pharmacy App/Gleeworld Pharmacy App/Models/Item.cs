e8c85c0d59d203ae9ec499da65c6989cae7b4e50
Samuel Williams <xsamms@outlook.com>
2020-03-06 21:43:17 +0100

    Gleeworld Pharmacy App Splash Screen

---
+++ Gleeworld Pharmacy App/Gleeworld Pharmacy App/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Gleeworld_Pharmacy_App.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Gleeworld Pharmacy App/Gleeworld Pharmacy App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Gleeworld_Pharmacy_App.Models;
+
+namespace Gleeworld_Pharmacy_App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
